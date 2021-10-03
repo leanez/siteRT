b3472a1df9766796711285ab194aa7d1627723e4
Leila Añez <leila.anez@ymail.com>
2021-10-03 13:47:02 -0300

    início da vida do botão

---
+++ js/adiciona-musica.js
@@ -0,0 +1,12 @@
+var musica1 = { música: "Jogging", nível: "S18", nota: "A", score: "432765"};
+var musica2 = { música: "Snapping", nível: "D18", nota: "SS", score: "997733"};
+var musica3 = { música: "Destrucimate", nível: "S16", nota: "A sem pass", score: "212121"};
+
+var musicas = [musica1, musica2, musica3];
+
+function exibeMusicaNaTabela(){
+    var novoElemento = "";
+    for(i = 0; i < ){
+
+    }
+}
