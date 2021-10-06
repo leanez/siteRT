0e22cee39916327e9700a0df7cc0f84d36a18215
Leila Añez <leila.anez@ymail.com>
2021-10-06 00:02:20 -0300

    injetando uma lista pronta

--- js/adiciona-musica.js
+++ js/adiciona-musica.js
@@ -4,9 +4,18 @@ var musica3 = { música: "Destrucimate", nível: "S16", nota: "A sem pass", scor
 
 var musicas = [musica1, musica2, musica3];
 
-function exibeMusicaNaTabela(){
+function exibeMusicaNaTabela(musicas){
     var novoElemento = "";
-    for(i = 0; i < ){
-
+    for(var i = 0; i < musicas.length; i++){
+        novoElemento = novoElemento + "<tr><td>" + musicas[i].música + "</td>";
+        novoElemento += "<td>" + musicas[i].nível + "</td>";
+        novoElemento += "<td>" + musicas[i].nota + "</td>";
+        novoElemento += "<td>" + musicas[i].score + "</td>";
+        novoElemento += "</tr>"
     }
+
+    var tabelaMusicas = document.getElementById("tabelaMusicas");
+    tabelaMusicas.innerHTML = novoElemento;
 }
+
+exibeMusicaNaTabela(musicas);
